9379f0fb3aba15288680e87fbc7604794c8cc854
Konstantin <kulkxx@gmail.com>
2022-08-28 00:59:45 +0700

    добавлены 4 задачи 34,36,38 и *

---
+++ zadacha zvezda lesson 5/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Урок 5.
+Задача "со звездочкой": Разобраться с алгоритмом сортировки методом пузырька.
+ Реализовать невозрастающую сортировку.
+[3, 0, 2, 4, -1] -> [4, 3, 2, 0, -1]
+[1,2,2,3,2] -> [3, 2, 2, 2, 1]
+*/
+
+Console.WriteLine("введите массив из 5 цифр");
+int [] array = new int [5];
+    for (int i = 0; i < 5; i++)
+    {
+        array[i] = int.Parse(Console.ReadLine());
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = i+1; j <5; j++)
+        {
+            if (array[i] < array[j])
+            { 
+                int n=array[i];
+                array[i] = array[j];
+                array[j] = n;
+            }
+   
+        }
+
+         Console.Write($"{array[i]}    ");
+    }
+Console.WriteLine();
---
+++ zadacha38/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Задача 38: Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76
+*/
+
+
+double[] array = new double [4];
+
+double max = array[1];
+double min = array[1];
+double res = 0;
+    for (int i = 0; i < 4; i++)
+    {
+    array[i] = new Random().Next(-1000,1000) + new Random().NextDouble();
+    array[i] = Math.Round(array[i] ,2);
+    Console.Write($"{array[i]}    ");
+
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    if (min < 0)
+    {
+        res = max + min;
+    }
+    else
+        res = max - min;
+
+Console.WriteLine();
+//Console.WriteLine($" максимальный {max}  минимальный {min}"); //вывод на экран для проверки
+Console.WriteLine($"разницу между максимальным и минимальным элементом массива: {res}");
+
