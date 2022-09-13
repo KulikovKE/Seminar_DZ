8c267974b28137bf7eb81405b8068f2f2dd4b8df
Konstantin <kulkxx@gmail.com>
2022-09-13 14:36:03 +0700

    добавлены задачи 41, 43 и *

---
+++ zadacha41/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3
+*/
+
+Console.WriteLine("укажите количество элементов в массиве");
+int size = int.Parse(Console.ReadLine());
+
+void Kolichestvo(int size)
+{
+int[] Myarray = new int[size];
+    for (int i=0; i<size; i++)
+    {
+        Console.WriteLine($"введите {i+1} элемент массива (положительный или отрицательный)");
+        Myarray[i] = int.Parse(Console.ReadLine());
+    }
+Console.WriteLine(String.Join(" ", Myarray));
+
+int kolPositive = 0;
+    foreach (int positive in Myarray)
+    {
+        if (positive > 0) 
+        kolPositive++;
+    } 
+
+Console.WriteLine($"Количество положительных элементов массива равно {kolPositive}");
+}
+Kolichestvo(size);
---
+++ zadacha43/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Задача 43. Напишите программу, которая найдёт точку 
+пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+ значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+
+
+Console.WriteLine("введите значения b1,k1 и b2,k2");
+double b1 = Convert.ToDouble(Console.ReadLine());
+double k1 = Convert.ToDouble(Console.ReadLine());
+double b2 = Convert.ToDouble(Console.ReadLine());
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+bool Check(double num1, double num2, double num3, double num4)
+    {
+    return num1==num3 && num2==num4 || num2==num4;
+    }
+    if(Check(b1, k1, b2, k2))
+    {
+        Console.WriteLine("прямые параллельны или совпадают");
+        return;
+    }
+else  
+    {
+        double x=(b2-b1)/(k1-k2);
+        double y = k1 * x + b1;
+        Console.WriteLine($"точка пересечения двух прямых ({x}, {y})");
+    }
+
